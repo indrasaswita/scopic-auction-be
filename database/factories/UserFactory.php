9760784657838bf600f855b314ffc2bbcea09be4
Indra Ganteng <indrasaswita@gmail.com>
2021-05-15 13:37:04 +0700

    edit laravel test featureTest

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,11 +23,15 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
+            'userkey' => 'user'.$this->faker->unique()->randomDigitNotNull(),
+            // 'email' => $this->faker->unique()->safeEmail(),
             'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
-            'remember_token' => Str::random(10),
+            'maxbidamount' => '200',
+            'gender' => 'Male',
+            'fund' => 120,
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 
